28d3741ab0fafae2e5ed99255329c85ece73306e
PRATIK AWASTHI <awasthipratik.98@gmail.com>
2021-08-17 19:58:29 +0530

    check for not null value updated

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -8,7 +8,47 @@ public class StringCalculator
         {
         	return 0;
         }   
-        return 0;
+        else 
+        {
+        	int sum=0;
+        	char[] ch = new char[numbers.length()];
+        	for(int i=0;i<numbers.length();i++)
+        	{
+        		ch[i]=numbers.charAt(i);
+        		if(ch[i]!='/' && ch[i]!=',' && ch[i]!='[' && ch[i]!=']' && ch[i]!='%' && ch[i]!='*' && ch[i]!='n' && ch[i]!='\n' && ch[i]!='\\' && ch[i]!=';')
+        		{
+        			int k=0;
+        			String negativenum="";
+        			if(ch[i]<0)
+        			{
+        				negativenum= negativenum+ch[i];
+        				k++;
+        			}
+        			if(k>0)
+        			{
+        				throw new RuntimeException("Negatives not allowed "+negativenum);
+        			}
+        			
+        			if(ch[i]=='+')
+        			{
+        		        sum = 0;
+        		        String[] aftersplit = numbers.split("[+]");
+        		 		for(String temp : aftersplit)
+        		 		{
+        		 			int tempint = Integer.parseInt(temp);
+        		 		    if(tempint<=1000)
+        		 		    {
+        		 		    	sum=sum+tempint;
+        		 		    }
+        		 	    }
+        				return sum;
+        			}
+        			
+        			sum = sum + Character.getNumericValue(ch[i]);
+        		}
+        	}
+        	return sum;
+        }
      }
 	
 	public int GetCalledCount()
